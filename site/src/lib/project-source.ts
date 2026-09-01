82dabd2b89616c20a04540d9811a767ed3e1e4e6
Didac Ll. <107514286+DidacLL@users.noreply.github.com>
2026-09-01 03:16:49 +0200

    Register AAAAT public site source

--- site/src/lib/project-source.ts
+++ site/src/lib/project-source.ts
@@ -17,6 +17,13 @@ export const projectSources: ProjectSource[] = [
     coverPath: ".acb/cover.png"
   },
   {
+    id: "aaaat",
+    repository: "DidacLL/AAAAT",
+    branch: "main",
+    documentPath: ".acb/site.md",
+    coverPath: ".acb/cover.svg"
+  },
+  {
     id: "ironbank",
     repository: "DidacLL/Ironhack-IronBank_FinalProject_vBNKsys",
     branch: "main",
